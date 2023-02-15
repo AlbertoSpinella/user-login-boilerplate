2a3671ed1816147b4bfc45713cd4051d00abea45
alberto.spinella@protonmail.com <alberto.spinella@protonmail.com>
2023-02-15 19:28:30 +0100

    add docker compose

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const mongodbEndpoint = "mongodb://127.0.0.1:27017";
+const mongodbEndpoint = "mongodb://mongodb:27017";
 
 export const mongooseConnect = () => {
     mongoose.set("strictQuery", true);
